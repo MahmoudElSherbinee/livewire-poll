cdcfb92197bae306b53c97ea43855c414adefced
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2026-01-11 06:11:18 +0200

    Creating the CreatePoll Component and apply real time validations and many things with livewire

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -2,13 +2,54 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
+    #[Validate('required|min:3|max:255')]
     public $title;
+
+    #[Validate([
+        'options' => 'required|array|min:1|max:10',
+        'options.*' => 'required|min:1|max:255'
+    ], message: [
+        'options.*' => "The option field can't be empty",
+        'options.max' => "One Poll can't have more than 10 options"
+    ])]
+    public $options = ['first'];
+
+
+    public function updated($propertyName){
+        $this->validateOnly($propertyName);
+    }
     public function render()
     {
         return view('livewire.create-poll');
+    }
+    public function createPoll()
+    {
+        $this->validate();
+        Poll::create([
+            'title' => $this->title,
+            'options' => $this->options
+        ])->options()->createMany(
+            collect($this->options)
+                ->map(fn($option) => ['name' => $option])
+                ->all()
+        );
+        $this->reset(['title', 'options']);
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
     }
 }
